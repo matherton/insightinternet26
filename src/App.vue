bb1ecc89b38e6457fa7cb6432ef6201a49cb087a
Mark Atherton <mark@insightinternet.co.uk>
2026-02-06 12:07:47 +0000

    first commit: Setup, Header, Footer and started re-creating HelloWorld

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <App-header />
+    <v-main>
+      <router-view />
+    </v-main>
+    <App-footer />
+  </v-app>
+</template>
+
+<script setup>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container class="fill-height d-flex align-center" max-width="900">
+    <div>
+      <v-img class="mb-4" height="150" src="@/assets/logo.png" />
+
+      <div class="mb-8 text-center">
+        <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+        <h1 class="text-h2 my-0 font-weight-bold">InsightInternet</h1>
+      </div>
+
+      <v-row>
+        <v-col v-for="link in links" :key="link.href" cols="6">
+          <v-card
+            append-icon="mdi-open-in-new"
+            class="py-4"
+            color="surface-variant"
+            :href="link.href"
+            :prepend-icon="link.icon"
+            rel="noopener noreferrer"
+            rounded="lg"
+            :subtitle="link.subtitle"
+            target="_blank"
+            :title="link.title"
+            variant="tonal"
+          />
+        </v-col>
+
+        <v-col cols="12">
+          <v-card
+            class="py-4"
+            color="surface-variant"
+            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+            prepend-icon="mdi-rocket-launch-outline"
+            rounded="lg"
+            variant="tonal"
+          >
+            <template #image>
+              <v-img position="top right" />
+            </template>
+
+            <template #title>
+              <h2 class="text-h5 font-weight-bold">Get started</h2>
+            </template>
+
+            <template #subtitle>
+              <div class="text-subtitle-1">
+                Transform your business with custom-built solutions tailored to
+                your needs.
+              </div>
+            </template>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script setup>
+const links = [
+  {
+    href: "https://vuetifyjs.com/",
+    icon: "mdi-information-outline",
+    subtitle: "Learn about all InsightInternet History and ethos.",
+    title: "About",
+  },
+  {
+    href: "https://vuetifyjs.com/introduction/why-vuetify/#feature-guides",
+    icon: "mdi-briefcase-outline",
+    subtitle: "Explore frameworks and Tools.",
+    title: "Solutions",
+  },
+  {
+    href: "https://vuetifyjs.com/components/all",
+    icon: "mdi-briefcase-account-outline",
+    subtitle: "Discover what projects have been built by InsightInternet.",
+    title: "Work",
+  },
+  {
+    href: "https://discord.vuetifyjs.com",
+    icon: "mdi-email-outline",
+    subtitle: "Connect with InsightInternet.",
+    title: "Contact",
+  },
+];
+</script>
