eb367e3be76fafc98791bf50b39aa5d8a028e2e6
Mark Atherton <mark@insightinternet.co.uk>
2026-02-10 13:12:38 +0000

    Finished intro paragraph

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,17 @@
       <div class="mb-8 text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h2 my-0 font-weight-bold">InsightInternet</h1>
+        <h2 class="text-grey mb-2">
+          Empowering Startups with Cutting-Edge Applications
+        </h2>
+        <p>
+          At InsightInternet, we specialise in delivering high-quality
+          applications to help startups succeed. Dedicated to providing bespoke
+          solutions that drive growth and efficiency as well as delivering
+          customised solutions without the constrants of cloud-based website
+          builders i.e. Wix, Squarespace ect.
+        </p>
       </div>
-      <v-row class="mb-8"> </v-row>
 
       <v-row>
         <v-col v-for="link in links" :key="link.href" cols="6">
@@ -15,7 +24,6 @@
             append-icon="mdi-open-in-new"
             class="py-4"
             color="surface-variant"
-            :href="link.href"
             :prepend-icon="link.icon"
             rel="noopener noreferrer"
             rounded="lg"
@@ -29,6 +37,7 @@
         <v-col cols="12">
           <v-card
             class="py-4"
+            href="#contact"
             color="surface-variant"
             image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
             prepend-icon="mdi-rocket-launch-outline"
@@ -87,8 +96,9 @@ const links = [
   {
     href: "https://vuetifyjs.com/",
     icon: "mdi-information-outline",
-    subtitle: "Learn about all InsightInternet History and ethos.",
-    title: "About",
+    subtitle:
+      "InsightInternet was founded with a vision to support startups by providing top-notch applications. Based in Edinburgh and partnering with Edinburgh startups and small businesses, we leverage the latest technologies to create seamless, responsive, and scalable applications.",
+    title: "Story",
   },
   {
     href: "https://vuetifyjs.com/introduction/why-vuetify/#feature-guides",
