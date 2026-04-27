82dfd5a7b0d0bc0bd93ea72db7d91a194c3c6d61
Mark Atherton <mark@insightinternet.co.uk>
2026-04-27 13:34:27 +0100

    replace me.png with me.webp

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -12,7 +12,7 @@
   />
   <v-container class="d-flex align-center" max-width="900" id="top">
     <div style="padding-top: 13.5rem">
-      <v-img class="mb-4" height="250" src="../assets/me-cropped.png" />
+      <v-img class="mb-4" height="250" src="../assets/me-cropped.webp" />
 
       <div class="mb-8 text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
